e9fd701b6d57e8a41909357bd1cf4bde62db7356
helmi akbar <helmi@41studio.com>
2013-11-15 17:31:59 +0700

    email

--- app/mailers/share_email.rb
+++ app/mailers/share_email.rb
@@ -1,7 +1,8 @@
 class ShareEmail < ActionMailer::Base
   default :from => "Weeverse"
-  def share_project(recipient, project_id)
+  def share_project(recipient, project_id, project_content)
   	@project = Project.find(project_id)
+  	@project_content = project_content
     mail(:to => recipient, :subject => "Weeverse Project Share")
   end
 end
